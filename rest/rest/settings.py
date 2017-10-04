260eea0e7ce7d4547c2f69f938abf9cb1e0d3ba2
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-05 09:58:29 +1100

    Default Permission class for app is now IsAuthenticatedOrReadOnly

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES' : [
        'rest_framework.authentication.TokenAuthentication'
